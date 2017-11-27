b3922ff1bb070c860d6f6566e4130bb3d0a29084
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-11-27 04:27:37 -0600

    misc styling

--- src/components/News.js
+++ src/components/News.js
@@ -19,7 +19,7 @@ class News extends React.Component {
                     return (
                         <div className="carousel-item active" key={index}>
                             <img className="carousel-image" src={url} alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
+                            <div className="carousel-caption">
                                 <h3>{article.headline.main}</h3>
                                 <p>{article.snippet}</p>
                             </div>
@@ -29,7 +29,7 @@ class News extends React.Component {
                     return (
                         <div className="carousel-item" key={index}>
                             <img className="carousel-image" src={url} alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
+                            <div className="carousel-caption">
                                 <h3>{article.headline.main}</h3>
                                 <p>{article.snippet}</p>
                             </div>
@@ -47,27 +47,27 @@ class News extends React.Component {
                 {
                     this.props.articles != null
                         ? (
-                            <div id="carouselIndicators" className="carousel slide" data-ride="carousel">
+                            <div id="article-carousel" className="carousel slide" data-ride="carousel">
                                 <ol className="carousel-indicators">
-                                    <li data-target="#carouselIndicators" data-slide-to="0" className="active"></li>
-                                    <li data-target="#carouselIndicators" data-slide-to="1"></li>
-                                    <li data-target="#carouselIndicators" data-slide-to="2"></li>
-                                    <li data-target="#carouselIndicators" data-slide-to="3"></li>
-                                    <li data-target="#carouselIndicators" data-slide-to="4"></li>
-                                    <li data-target="#carouselIndicators" data-slide-to="5"></li>
-                                    <li data-target="#carouselIndicators" data-slide-to="6"></li>
-                                    <li data-target="#carouselIndicators" data-slide-to="7"></li>
-                                    <li data-target="#carouselIndicators" data-slide-to="8"></li>
-                                    <li data-target="#carouselIndicators" data-slide-to="9"></li>
+                                    <li data-target="#article-carousel" data-slide-to="0" className="active"></li>
+                                    <li data-target="#article-carousel" data-slide-to="1"></li>
+                                    <li data-target="#article-carousel" data-slide-to="2"></li>
+                                    <li data-target="#article-carousel" data-slide-to="3"></li>
+                                    <li data-target="#article-carousel" data-slide-to="4"></li>
+                                    <li data-target="#article-carousel" data-slide-to="5"></li>
+                                    <li data-target="#article-carousel" data-slide-to="6"></li>
+                                    <li data-target="#article-carousel" data-slide-to="7"></li>
+                                    <li data-target="#article-carousel" data-slide-to="8"></li>
+                                    <li data-target="#article-carousel" data-slide-to="9"></li>
                                 </ol>
                                 <div className="carousel-inner">
                                     {this.renderArticles()}
                                 </div>
-                                <a className="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                                <a className="carousel-control-prev" href="#article-carousel" role="button" data-slide="prev">
                                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                                     <span className="sr-only">Previous</span>
                                 </a>
-                                <a className="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+                                <a className="carousel-control-next" href="#article-carousel" role="button" data-slide="next">
                                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span className="sr-only">Next</span>
                                 </a>
